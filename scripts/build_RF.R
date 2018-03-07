0dbc216e9110985900fb59724f82385d48bfa348
David McGaughey <mcgaughey@gmail.com>
2018-03-06 22:15:33 -0500

    more models added, save loop added

--- scripts/build_RF.R
+++ scripts/build_RF.R
@@ -15,11 +15,16 @@ all_processed <- uk10k_gemini_rare_variants %>%
                                      impact_severity == 'MED' ~ 2, 
                                      TRUE ~ 1),
          Status = case_when(Status=='Pathogenic' ~ 'Pathogenic',
-                            TRUE ~ 'NotPathogenic')) %>% 
+                            TRUE ~ 'NotPathogenic'),
+         genesplicer = case_when(genesplicer == "" ~ 'No',
+                                 grepl('^gain', genesplicer) ~ 'Gain',
+                                 grepl('^loss', genesplicer) ~ 'Loss',
+                                 grepl('^diff', genesplicer) ~ 'Diff',
+                                 TRUE ~ 'Else')) %>% 
   mutate(Status = factor(Status, levels=c('Pathogenic','NotPathogenic'))) %>% 
   mutate_at(vars(matches('ac_|an_|^n_')), funs(as.integer(.))) %>% # convert columns with ac_|whatever to integer (ac is allele count)
   mutate_at(vars(matches('af_|dann|revel|mpc|gerp|polyphen_score|sift_score|fitcons_float|gerp_elements|^adj|_z$|^pli$|^pnull$|precessive|^phylop')), funs(as.numeric(.))) %>%  # af is allele frequency
-  select(variant_id, Status, Complicated_Status, is_exonic, is_coding, is_lof, is_splicing, impact_severity, polyphen_score, sift_score, dann, gerp_elements, DiseaseClass, mpc, revel, aaf_1kg_afr_float:an_exac_sas, fitcons_float, gno_ac_afr:gno_an_popmax, lof_z:precessive, phylop_100way, grantham, maxentscan, cadd_phred, fathmm_mkl_coding_score, genesplicer, spliceregion) %>% 
+  select(variant_id, Status, Complicated_Status, is_exonic, is_coding, is_lof, is_splicing, impact_severity, polyphen_score, sift_score, dann, gerp_elements, DiseaseClass, mpc, revel, aaf_1kg_afr_float:an_exac_sas, fitcons_float, gno_ac_afr:gno_an_popmax, lof_z:precessive, phylop_100way, grantham, cadd_phred, fathmm_mkl_coding_score, genesplicer, spliceregion) %>% 
   filter(max_aaf_all < 0.01) %>% 
   unique() # remove any common variants
 
@@ -64,7 +69,7 @@ clinvar_processed <- clinvar %>%
   mutate(Status = factor(Status, levels=c('Pathogenic','NotPathogenic'))) %>% 
   mutate_at(vars(matches('ac_|an_|^n_')), funs(as.integer(.))) %>% # convert columns with ac_|whatever to integer (ac is allele count)
   mutate_at(vars(matches('af_|dann|revel|mpc|gerp|polyphen_score|sift_score|fitcons_float|gerp_elements|^adj|_z$|^pli$|^pnull$|precessive|^phylop_100')), funs(as.numeric(.))) %>%  # af is allele frequency
-  select(variant_id, Status, is_exonic, is_coding, is_lof, is_splicing, impact_severity, polyphen_score, sift_score, dann, gerp_elements, DiseaseClass, mpc, revel, aaf_1kg_afr_float:an_exac_sas, fitcons_float, gno_ac_afr:gno_an_popmax, lof_z:precessive, phylop_100way, grantham, maxentscan, cadd_phred, fathmm_mkl_coding_score, genesplicer, spliceregion) %>% 
+  select(variant_id, Status, is_exonic, is_coding, is_lof, is_splicing, impact_severity, polyphen_score, sift_score, dann, gerp_elements, DiseaseClass, mpc, revel, aaf_1kg_afr_float:an_exac_sas, fitcons_float, gno_ac_afr:gno_an_popmax, lof_z:precessive, phylop_100way, grantham, cadd_phred, fathmm_mkl_coding_score, genesplicer, spliceregion) %>% 
   filter(max_aaf_all < 0.01) # remove any common variants
 
 clinvar_processed[is.na(clinvar_processed)] <- -1
@@ -89,15 +94,22 @@ ML_set__clinvar <- rbind(all_PATH, all_NOT_PATH__CUT)
 # Combine UK10K and ClinVar data
 ML_set__all <- bind_rows(ML_set__clinvar %>% select_(.dots = colnames(ML_set__UK10K %>% select(-Complicated_Status))), ML_set__UK10K %>% select(-Complicated_Status))
 
+# one hot encode
+library(dummies)
+temp <- ML_set__all %>% dplyr::select(-Status, -Source, -variant_id)
+temp <- dummy.data.frame(temp, sep='_')
+ML_set_dummy <- temp %>% mutate(variant_id = ML_set__all$variant_id, Status = ML_set__all$Status, Source = ML_set__all$Source)
 
 set.seed(115470)
-train_set <- ML_set__all %>% 
+train_set <- ML_set_dummy %>% 
   group_by(Status, Source) %>% 
   # filter(!Complicated_Status=='Comp_Het') %>% # remove comp hets for now
   sample_frac(0.33) %>% ungroup()
 
+train_set_CS <-  preProcess(train_set, method = c('center','scale')) %>% predict(., train_set)
+
 set.seed(115470)
-validate_set <- ML_set__all %>% 
+validate_set <- ML_set_dummy %>% 
   filter(!variant_id %in% train_set$variant_id) %>% 
   group_by(Status, Source) %>% 
   sample_frac(0.5) %>% ungroup()
@@ -117,18 +129,128 @@ fitControl_RF <- trainControl(
   summaryFunction = prSummary,
   returnData = T)
 
+fitControl <- trainControl(## 5-fold CV
+  method = "cv",
+  number = 5,
+  classProbs=T,
+  savePredictions = T,
+  allowParallel = T,
+  summaryFunction = prSummary,
+  returnData = T)
 
-cluster <- makeCluster(24) 
+fitControl_min <- trainControl(
+  classProbs=T,
+  savePredictions = T,
+  allowParallel = T,
+  summaryFunction = prSummary,
+  returnData = T)
+
+cluster <- makeCluster(30) 
 registerDoParallel(cluster)
 
 rfFit <- caret::train(Status ~ ., data=train_set %>% select(-variant_id, -Source), 
                       preProcess = c("scale", "center"),
                       method = "rf", metric='F',
                       trControl = fitControl_RF)
-save(rfFit, file='rfFit_2018_03_03.Rdata')
 
-rfFit_noDC <- caret::train(Status ~ ., data=train_set %>% select(-variant_id, -Source, -DiseaseClass), 
+rfFit_noDC <- caret::train(Status ~ ., data=train_set %>% select(-variant_id, -Source, -contains('DiseaseClass')), 
                       preProcess = c("scale", "center"),
                       method = "rf", metric='F',
                       trControl = fitControl_RF)
-save(rfFit_noDC, file='rfFit_noDC_2018_03_03.Rdata')
+
+bglmFit <- caret::train(Status ~ ., data=train_set %>% select(-variant_id, -Source), 
+                        preProcess = c("scale", "center"),
+                        method = "bayesglm", metric='F',
+                        trControl = fitControl)
+
+# check again, had errors when running
+glmboostFit <- caret::train(Status ~ ., data=train_set %>% select(-variant_id, -Source),
+                            preProcess = c("scale", "center"),
+                            method = "glmboost", metric='F',
+                            trControl = fitControl)
+
+LogitBoostFit <- caret::train(Status ~ ., data=train_set %>% select(-variant_id, -Source), 
+                              preProcess = c("scale", "center"),
+                              method = "LogitBoost", metric='F',
+                              trControl = fitControl)
+
+avNNetFit <- caret::train(Status ~ ., data=train_set %>% select(-variant_id, -Source), 
+                          preProcess = c("scale", "center"),
+                          method = "avNNet", metric='F',
+                          trControl = fitControl)
+
+# tossed, terrible performance
+# xgbTreeFit <- caret::train(Status ~ ., data=train_set %>% select(-variant_id, -Source), 
+#                       preProcess = c("scale", "center"),
+#                       method = "xgbTree",  metric='AUC',
+#                       trControl = fitControl)
+
+stepLDAFit <- caret::train(Status ~ ., data=train_set %>% select(-variant_id, -Source), 
+                           preProcess = c("scale", "center"),
+                           method = "stepLDA",  metric='AUC',
+                           trControl = fitControl)
+
+# tossed, terrible performance
+# naive_bayesFit <- caret::train(Status ~ ., data=train_set %>% select(-variant_id, -Source), 
+#                       preProcess = c("scale", "center"),
+#                       method = "naive_bayes",  metric='AUC',
+#                       trControl = fitControl)
+
+dnnFit <- caret::train(Status ~ ., data=train_set %>% select(-variant_id, -Source), 
+                       preProcess = c("scale", "center"),
+                       method = "dnn",  metric='AUC',
+                       trControl = fitControl)
+
+monmlpFit <- caret::train(Status ~ ., data=train_set %>% select_(.dots=c('Status',most_imp_predictors)), 
+                          preProcess = c("scale", "center"),
+                          method = "monmlp",  metric='F',
+                          trControl = fitControl_min)
+
+# mlpKerasDropoutFit <- caret::train(x = train_set %>% select(-variant_id, -Source, -Status),
+#                                  y= train_set$Status,
+#                       preProcess = c("scale", "center"),
+#                       method = "mlpKerasDropout",  metric='F',
+#                       trControl = fitControl)
+
+
+svmLinearWeightsFit <- caret::train(Status ~ ., data=train_set %>% select(-variant_id, -Source), 
+                                    preProcess = c("scale", "center"),
+                                    method = "svmLinearWeights",  metric='Precision',
+                                    trControl = fitControl)
+
+caddFit <- caret::train(Status ~ ., data=train_set %>% select(cadd_phred, Status), 
+                        preProcess = c("scale", "center"),
+                        method = "glm",  metric='Precision',
+                        trControl = fitControl)
+
+cadd_plus_DiseaseClassFit <- caret::train(Status ~ ., data=train_set %>% select(cadd_phred, Status, `DiseaseClass_-1`), 
+                                          preProcess = c("scale", "center"),
+                                          method = "glm",  metric='Precision',
+                                          trControl = fitControl)
+
+revelFit <- caret::train(Status ~ ., data=train_set %>% select(revel, Status), 
+                         preProcess = c("scale", "center"),
+                         method = "glm",
+                         trControl = fitControl)
+
+revelFit_plus_DiseaseClassFit <- caret::train(Status ~ ., data=train_set %>% select(revel, Status, `DiseaseClass_-1`), 
+                                              preProcess = c("scale", "center"),
+                                              method = "glm",
+                                              trControl = fitControl)
+
+dannFit <- caret::train(Status ~ ., data=train_set %>% select(dann, Status), 
+                        preProcess = c("scale", "center"),
+                        method = "glm",
+                        trControl = fitControl)
+
+dann_plus_DiseaseClassFit <- caret::train(Status ~ ., data=train_set %>% select(dann, Status, `DiseaseClass_-1`), 
+                                          preProcess = c("scale", "center"),
+                                          method = "glm",
+                                          trControl = fitControl)
+
+my_models <-list()
+for (i in ls()[grepl('Fit',ls())]) {my_models[[i]] <- get(i)}
+
+
+
+most_imp_predictors <- varImp(rfFit)$importance  %>% rownames_to_column('Predictors') %>% arrange(-Overall) %>% filter(Overall > 2) %>% pull(Predictors)
