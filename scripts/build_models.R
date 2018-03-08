d1fad62d83b096d4fe07562905dade2eeb070fab
David McGaughey <mcgaughey@gmail.com>
2018-03-08 16:19:12 -0500

    added processing for gnomad benign variants, saves train - validate - test sets in output models

--- scripts/build_models.R
+++ scripts/build_models.R
@@ -5,14 +5,20 @@
 args = commandArgs(trailingOnly=TRUE)
 uk10k_file <- args[1]
 clinvar_file <- args[2]
+gnomad_file <- args[3]
 
+library(tidyverse)
+library(data.table)
+
+
+###############################
+# UK10K processing
+###############################
 # Load data from ~/git/EGA_EGAD00001002656_NGS_reanalyze/scripts/stats.Rmd
 #load('uk10k_gemini_rare_variants.Rdata')
 load(uk10k_data)
 
 ## Set up data for modeling
-library(tidyverse)
-
 all_processed <- uk10k_gemini_rare_variants %>% 
   separate(gene_eyediseaseclass, c('RDGene','DiseaseClass'), sep='_') %>%  #split off RD disease type
   select(-RDGene) %>% 
@@ -54,8 +60,9 @@ all_NOT_PATH__CUT <- all_set__uk10k %>% filter(Status=='NotPathogenic') #%>% sam
 
 ML_set__UK10K <- rbind(all_PATH, all_NOT_PATH__CUT)
 
-# Load clinvar 
-library(data.table)
+###########################################
+# ClinVar  Processing
+###########################################
 #clinvar <- fread('zcat ~/git/eye_var_Pathogenicity/processed_data/clinvar.gemini.tsv.gz')
 clinvar <- fread(paste0('zcat ', clinvar_file))
 
@@ -99,24 +106,72 @@ all_NOT_PATH__CUT <- all_set__clinvar %>% filter(Status=='NotPathogenic')
 
 ML_set__clinvar <- rbind(all_PATH, all_NOT_PATH__CUT)
 
-# Combine UK10K and ClinVar data
-ML_set__all <- bind_rows(ML_set__clinvar %>% select_(.dots = colnames(ML_set__UK10K %>% select(-Complicated_Status))), ML_set__UK10K %>% select(-Complicated_Status))
+
+###############################################
+# gnomAD benign? processing
+##############################################
+gnomad <- fread(paste0('zcat ', gnomad_file))
+## Prep data for modeling
+gnomad_processed <- gnomad %>% 
+  separate(gene_eyediseaseclass, c('RDGene','DiseaseClass'), sep='_') %>%  #split off RD disease type
+  select(-RDGene) %>% 
+  mutate(impact_severity = case_when(impact_severity == 'HIGH' ~ 3, # convert to integer 
+                                     impact_severity == 'MED' ~ 2, 
+                                     TRUE ~ 1),
+         Status = 'NotPathogenic',
+         genesplicer = case_when(genesplicer == "" ~ 'No',
+                                 grepl('^gain', genesplicer) ~ 'Gain',
+                                 grepl('^loss', genesplicer) ~ 'Loss',
+                                 grepl('^diff', genesplicer) ~ 'Diff',
+                                 TRUE ~ 'Else')) %>% 
+  mutate(Status = factor(Status, levels=c('Pathogenic','NotPathogenic'))) %>% 
+  filter(hgmd_overlap=='None' & clinvar_pathogenic == 'None') %>% # remove possible pathogenic by checking against hgmd or clinvar presence
+  mutate_at(vars(matches('ac_|an_|^n_')), funs(as.integer(.))) %>% # convert columns with ac_|whatever to integer (ac is allele count)
+  mutate_at(vars(matches('af_|dann|revel|mpc|gerp|polyphen_score|sift_score|fitcons_float|gerp_elements|^adj|_z$|^pli$|^pnull$|precessive|^phylop_100')), funs(as.numeric(.))) %>%  # af is allele frequency
+  select(variant_id, Status, is_exonic, is_coding, is_lof, is_splicing, impact_severity, polyphen_score, sift_score, dann, gerp_elements, DiseaseClass, mpc, revel, aaf_1kg_afr_float:an_exac_sas, fitcons_float, gno_ac_afr:gno_an_popmax, lof_z:precessive, phylop_100way, grantham, cadd_phred, fathmm_mkl_coding_score, genesplicer, spliceregion) %>% 
+  filter(max_aaf_all < 0.01) # remove any common variants
+
+# fill missing with -1
+gnomad_processed[is.na(gnomad_processed)] <- -1
+
+# add 250X the number of clinvar pathogenic variants 
+set.seed(13457)
+gnomad_processed_sub <- gnomad_processed %>% sample_n(ML_set__clinvar %>% filter(Status=='Pathogenic') %>% nrow()  * 250)
+
+
+################################################
+# Combine UK10K and ClinVar and gnomAD data
+################################################
+
+ML_set__all <- bind_rows(ML_set__clinvar %>% select_(.dots = colnames(ML_set__UK10K %>% select(-Complicated_Status))), 
+                         ML_set__UK10K %>% select(-Complicated_Status),
+                         gnomad_processed_sub %>% select(.dots = colnames(ML_set__UK10K)))
 
 # one hot encode
 library(dummies)
 temp <- ML_set__all %>% dplyr::select(-Status, -Source, -variant_id)
 temp <- dummy.data.frame(temp, sep='_')
 ML_set_dummy <- temp %>% mutate(variant_id = ML_set__all$variant_id, Status = ML_set__all$Status, Source = ML_set__all$Source)
+# center scale
+ML_set_dummy_CS <- preProcess(ML_set_dummy, method = c('center','scale')) %>% predict(., ML_set_dummy)
 
 set.seed(115470)
-train_set <- ML_set_dummy %>% 
+train_set <- ML_set_dummy_CS %>% 
   group_by(Status, Source) %>% 
   # filter(!Complicated_Status=='Comp_Het') %>% # remove comp hets for now
   sample_frac(0.33) %>% ungroup()
 
-# center scale
-library(caret)
-train_set_CS <-  preProcess(train_set, method = c('center','scale')) %>% predict(., train_set)
+set.seed(115470)
+validate_set <- ML_set_dummy_CS %>% 
+  filter(!variant_id %in% train_set$variant_id) %>% 
+  group_by(Status, Source) %>% 
+  sample_frac(0.5) %>% ungroup()
+
+test_set <- all_processed %>% 
+  filter(!variant_id %in% c(train_set$variant_id, validate_set$variant_id))
+
+
+
 
 # set.seed(115470)
 # validate_set <- ML_set_dummy %>% 
@@ -172,16 +227,16 @@ rfFit <- caret::train(Status ~ ., data=train_set_CS %>% select_(.dots=c('Status'
                       trControl = fitControl_RF)
 
 rfFit_noDC <- caret::train(Status ~ ., data=train_set_CS %>% select_(.dots=c('Status',most_imp_predictors_no_disease_class)), 
-                      method = "rf", metric='F',
-                      trControl = fitControl_RF)
+                           method = "rf", metric='F',
+                           trControl = fitControl_RF)
 
 bglmFit <- caret::train(Status ~ ., data=train_set_CS %>% select_(.dots=c('Status',most_imp_predictors)), 
                         method = "bayesglm", metric='F',
                         trControl = fitControl)
 
 bglmFit_noDC <- caret::train(Status ~ ., data=train_set_CS %>% select_(.dots=c('Status',most_imp_predictors_no_disease_class)), 
-                        method = "bayesglm", metric='F',
-                        trControl = fitControl)
+                             method = "bayesglm", metric='F',
+                             trControl = fitControl)
 
 # check again, had errors when running
 glmboostFit <- caret::train(Status ~ ., data=train_set_CS %>% select_(.dots=c('Status',most_imp_predictors)), 
@@ -197,8 +252,8 @@ avNNetFit <- caret::train(Status ~ ., data=train_set_CS %>% select_(.dots=c('Sta
                           trControl = fitControl)
 
 avNNetFit_noDC <- caret::train(Status ~ ., data=train_set_CS %>% select_(.dots=c('Status',most_imp_predictors_no_disease_class)), 
-                          method = "avNNet", metric='F',
-                          trControl = fitControl)
+                               method = "avNNet", metric='F',
+                               trControl = fitControl)
 
 # tossed, terrible performance
 # xgbTreeFit <- caret::train(Status ~ ., data=train_set_CS %>% select(-variant_id, -Source), 
@@ -254,8 +309,8 @@ revelFit <- caret::train(Status ~ ., data=train_set_CS %>% select(revel, Status)
                          trControl = fitControl)
 
 revel_plus_DiseaseClassFit <- caret::train(Status ~ ., data=train_set_CS %>% select(revel, Status, `DiseaseClass_-1`), 
-                                              method = "glm",
-                                              trControl = fitControl)
+                                           method = "glm",
+                                           trControl = fitControl)
 
 dannFit <- caret::train(Status ~ ., data=train_set_CS %>% select(dann, Status), 
                         method = "glm",
@@ -269,4 +324,7 @@ my_models <-list()
 for (i in ls()[grepl('Fit',ls())]) {my_models[[i]] <- get(i)}
 my_models$most_imp_predictors <- most_imp_predictors
 my_models$most_imp_predictors_no_disease_class <- most_imp_predictors_no_disease_class
+my_models$train_set <- train_set 
+my_models$validate_set <- validate_set
+my_models$test_set <- test_set
 save(my_models, file='eye_var_path_models__2018_03_08.Rdata')
