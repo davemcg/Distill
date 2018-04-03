be3aa5dc573a64348a572ed69ceeb2123d92421a
David McGaughey <mcgaughey@gmail.com>
2018-04-03 17:03:00 -0400

    unifun prep and annotation

---
+++ scripts/build_unifun.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd /data/mcgaugheyd/projects/nei/mcgaughey/eye_var_Pathogenicity/data/unifun
+wget https://ndownloader.figshare.com/articles/4704955/versions/1
+unzip 1
+
+/home/mcgaugheyd/git/eye_var_Pathogenicity/scripts/vcf_add_fake_sample.py UniFun_deleterious.vcf | bgzip > UniFun_deleterious.fakeS.vcf.gz
+/home/mcgaugheyd/git/eye_var_Pathogenicity/scripts/vcf_add_fake_sample.py UniFun_benign.vcf | bgzip > UniFun_benign.fakeS.vcf.gz
+
+tabix -p vcf UniFun_deleterious.fakeS.vcf.gz
+tabix -p vcf UniFun_benign.fakeS.vcf.gz
+
+bash ~/git/variant_prioritization/Snakemake.wrapper.sh ~/git/eye_var_Pathogenicity/config_variant_prioritization__unifunD.yaml
+bash ~/git/variant_prioritization/Snakemake.wrapper.sh ~/git/eye_var_Pathogenicity/config_variant_prioritization__unifunB.yaml
