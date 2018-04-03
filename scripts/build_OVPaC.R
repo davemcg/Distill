76c2437b9632563725903f426cd547ddad54dc6a
David McGaughey <mcgaughey@gmail.com>
2018-04-03 10:57:16 -0400

    making like VPaC to speed up compute time

--- scripts/build_OVPaC.R
+++ scripts/build_OVPaC.R
@@ -1,12 +1,20 @@
+########################
+# Build VPaC
+# generic pathogenicity score
+#########################
+
+
+############################
+# Only makes 33 trees
+# Run 50 times and combine the 50 33-tree forests with `randomForest::combine`
+#############################
+
+############################
+# give 8GB for biowulf2 run
+############################
 
 library(tidyverse)
-library(data.table)
-library(dummies)
-library(caret)
-library(mlbench)
-library(parallel)
-library(doParallel)
-library(MLmetrics)
+library(randomForest)
 
 load('/data/mcgaugheyd/projects/nei/mcgaughey/eye_var_Pathogenicity/clean_data/model_data.Rdata')
 
@@ -27,24 +35,25 @@ fitControl_min <- trainControl(
 most_imp_predictors <- c('is_lof','impact_severity','`DiseaseClass_-1`','mis_z','ccr_pct_v1','cadd_phred','phylop_100way','n_mis','revel','fitcons_float','precessive','n_lof','`DiseaseClass_Stargardt,RD`','m_cap_rankscore','dann','DiseaseClass_RD','vest3_rankscore','n_syn','pnull','pli','lof_z','fathmm_mkl_coding_rankscore','an_exac_all','eigen_pc_raw_rankscore','gerp_elements','mutationassessor_score_rankscore','mpc','metasvm_rankscore','polyphen_score','metalr_rankscore','lrt_converted_rankscore','genocanyon_score_rankscore','mutationtaster_converted_rankscore','gno_an_popmax','grantham','max_aaf_all','ac_exac_all','fathmm_converted_rankscore','aaf_esp_all','sift_score','ac_exac_sas', 'linsight')
 most_imp_predictors_no_disease_class <- c('is_lof','impact_severity','mis_z','ccr_pct_v1','cadd_phred','phylop_100way','n_mis','revel','fitcons_float','precessive','n_lof','m_cap_rankscore','dann','vest3_rankscore','n_syn','pnull','pli','lof_z','fathmm_mkl_coding_rankscore','an_exac_all','eigen_pc_raw_rankscore','gerp_elements','mutationassessor_score_rankscore','mpc','metasvm_rankscore','polyphen_score','metalr_rankscore','lrt_converted_rankscore','genocanyon_score_rankscore','mutationtaster_converted_rankscore','gno_an_popmax','grantham','max_aaf_all','ac_exac_all','fathmm_converted_rankscore','aaf_esp_all','sift_score','ac_exac_sas','linsight')
 
-###########################################
-# multi processing
-##########################################
-cluster <- makeCluster(10) 
-registerDoParallel(cluster)
 
 ##############################################
-# BUILD MODELS!!!!!!!!!!!
+# BUILD MODEL!!!!!!!!!!!
 #############################################
 
+rand_num <- as.integer(paste(sample(0:9, 6, replace=F), collapse = ''))
+set.seed(rand_num)
+model_data$ML_set__eye_dummy_TT$train_set$Status <- factor(model_data$ML_set__eye_dummy_TT$train_set$Status, levels=c('Pathogenic','NotPathogenic'))
+rfFit_OVPaC <- randomForest(Status ~ ., data=model_data$ML_set__eye_dummy_TT$train_set %>% select_(.dots=c('Status',most_imp_predictors)), 
+                            ntree=33,
+                            mtry=22,
+                            importance = TRUE,
+                            norm.votes = FALSE)
 
-rfFit_OVPaC <- caret::train(Status ~ ., data=model_data$ML_set__eye_dummy_TT$train_set %>% select_(.dots=c('Status',most_imp_predictors)), 
-                            method = "rf", metric='F', ntree=1501,
-                            trControl = fitControl_min)
+rand_name = paste0('OVPaC__', rand_num)
+assign(rand_name, rfFit_OVPaC)
 
 
 ##############################
 # SAVE MODEL
 ###############################
-
-save(rfFit_OVPaC, file='/data/mcgaugheyd/projects/nei/mcgaughey/eye_var_Pathogenicity/clean_data/rfFit_OVPaC.Rdata')
+save(rand_name, file=paste0('/data/mcgaugheyd/projects/nei/mcgaughey/eye_var_Pathogenicity/clean_data/OVPaC_pieces/', rand_name, '.Rdata'))
