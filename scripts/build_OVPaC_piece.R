65e32955ca00e6e7251f448cf6559105e57e5317
David McGaughey <mcgaughey@gmail.com>
2018-04-03 14:09:25 -0400

    tweak to use non-dummy data

--- scripts/build_OVPaC_piece.R
+++ scripts/build_OVPaC_piece.R
@@ -33,7 +33,7 @@ fitControl_min <- trainControl(
 ######################################
 # Predictors
 #######################################
-most_imp_predictors <- c('is_lof','impact_severity','`DiseaseClass_-1`','mis_z','ccr_pct_v1','cadd_phred','phylop_100way','n_mis','revel','fitcons_float','precessive','n_lof','`DiseaseClass_Stargardt,RD`','m_cap_rankscore','dann','DiseaseClass_RD','vest3_rankscore','n_syn','pnull','pli','lof_z','fathmm_mkl_coding_rankscore','an_exac_all','eigen_pc_raw_rankscore','gerp_elements','mutationassessor_score_rankscore','mpc','metasvm_rankscore','polyphen_score','metalr_rankscore','lrt_converted_rankscore','genocanyon_score_rankscore','mutationtaster_converted_rankscore','gno_an_popmax','grantham','max_aaf_all','ac_exac_all','fathmm_converted_rankscore','aaf_esp_all','sift_score','ac_exac_sas', 'linsight')
+most_imp_predictors <- c('is_lof','impact_severity','DiseaseClass','mis_z','ccr_pct_v1','cadd_phred','phylop_100way','n_mis','revel','fitcons_float','precessive','n_lof','m_cap_rankscore','dann','vest3_rankscore','n_syn','pnull','pli','lof_z','fathmm_mkl_coding_rankscore','an_exac_all','eigen_pc_raw_rankscore','gerp_elements','mutationassessor_score_rankscore','mpc','metasvm_rankscore','polyphen_score','metalr_rankscore','lrt_converted_rankscore','genocanyon_score_rankscore','mutationtaster_converted_rankscore','gno_an_popmax','grantham','max_aaf_all','ac_exac_all','fathmm_converted_rankscore','aaf_esp_all','sift_score','ac_exac_sas', 'linsight')
 most_imp_predictors_no_disease_class <- c('is_lof','impact_severity','mis_z','ccr_pct_v1','cadd_phred','phylop_100way','n_mis','revel','fitcons_float','precessive','n_lof','m_cap_rankscore','dann','vest3_rankscore','n_syn','pnull','pli','lof_z','fathmm_mkl_coding_rankscore','an_exac_all','eigen_pc_raw_rankscore','gerp_elements','mutationassessor_score_rankscore','mpc','metasvm_rankscore','polyphen_score','metalr_rankscore','lrt_converted_rankscore','genocanyon_score_rankscore','mutationtaster_converted_rankscore','gno_an_popmax','grantham','max_aaf_all','ac_exac_all','fathmm_converted_rankscore','aaf_esp_all','sift_score','ac_exac_sas','linsight')
 
 
@@ -43,8 +43,8 @@ most_imp_predictors_no_disease_class <- c('is_lof','impact_severity','mis_z','cc
 
 rand_num <- as.integer(paste(sample(0:9, 6, replace=F), collapse = ''))
 set.seed(rand_num)
-model_data$ML_set__eye_dummy_TT$train_set$Status <- factor(model_data$ML_set__eye_dummy_TT$train_set$Status, levels=c('Pathogenic','NotPathogenic'))
-rfFit_OVPaC <- randomForest(Status ~ ., data=model_data$ML_set__eye_dummy_TT$train_set %>% select_(.dots=c('Status',most_imp_predictors)), 
+
+rfFit_OVPaC <- randomForest(Status ~ ., data=model_data$ML_set__eye_TT$train_set %>% select_(.dots=c('Status',most_imp_predictors)), 
                             ntree=33,
                             mtry=22,
                             importance = TRUE,
