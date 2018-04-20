7cd305bc50b1aa12039a49cd255f2628a335deb3
David McGaughey <mcgaughey@gmail.com>
2018-04-20 16:59:54 -0400

    more general to handle more models with dif mtry

--- scripts/merge_models.R
+++ scripts/merge_models.R
@@ -4,23 +4,32 @@
 library(randomForest)
 library(tidyverse)
 
-#VPaC
-vpac_files <- list.files('/Volumes/data/projects/nei/mcgaughey/eye_var_Pathogenicity/clean_data/VPaC_pieces/', pattern = '*Rdata', full.names = TRUE)
-for (i in vpac_files){load(i)}
-ls()[grepl('VPaC', ls())]
+model_merger <- function(pattern='VPaC__6mtry_v2*', mtry=6, pac = 'VPaC_pieces', type='VPaC'){
+  files <- list.files(paste0('/Volumes/data/projects/nei/mcgaughey/eye_var_Pathogenicity/clean_data/', pac), pattern = pattern, full.names = TRUE)
+  for (i in files){load(i)}
+  ls()[grepl('VPaC', ls())]
+  
+  pac_list <- list()
+  for (i in ls()[grepl('VPaC', ls())]){pac_list[[i]] <- get(i)}
+  PaC <- do.call(randomForest::combine, pac_list)
+  
+  
+  name = paste0(type, '_', mtry, 'mtry')
+  assign(name, PaC)
+  ##############################
+  # SAVE MODEL
+  ###############################
+  save(list=name, file=paste0('/Volumes/data/projects/nei/mcgaughey/eye_var_Pathogenicity/clean_data/', name, '.Rdata'))
+}
 
-vpac_list <- list()
-for (i in ls()[grepl('VPaC', ls())]){vpac_list[[i]] <- get(i)}
-VPaC <- do.call(randomForest::combine, vpac_list)
-save(VPaC, file ='/Volumes/data/projects/nei/mcgaughey/eye_var_Pathogenicity/clean_data/VPaC_RF.Rdata')
+#VPaC
+model_merger(pattern='VPaC__3mtry_v2*', mtry=3, pac='VPaC_pieces', type='VPaC')
+model_merger(pattern='VPaC__6mtry_v2*', mtry=6, pac='VPaC_pieces', type='VPaC')
+model_merger(pattern='VPaC__10mtry_v2*', mtry=10, pac='VPaC_pieces', type='VPaC')
 
 #OVPaC
-ovpac_files <- list.files('/Volumes/data/projects/nei/mcgaughey/eye_var_Pathogenicity/clean_data/OVPaC_pieces/', pattern = '*Rdata', full.names = TRUE)
-for (i in ovpac_files){load(i)}
-ls()[grepl('OVPaC', ls())]
+model_merger(pattern='OVPaC__3mtry_v2*', mtry=3, pac='OVPaC_pieces', type='OVPaC')
+model_merger(pattern='OVPaC__6mtry_v2*', mtry=6, pac='OVPaC_pieces', type='OVPaC')
+model_merger(pattern='OVPaC__10mtry_v2*', mtry=10, pac='OVPaC_pieces', type='OVPaC')
 
-ovpac_list <- list()
-for (i in ls()[grepl('OVPaC', ls())]){ovpac_list[[i]] <- get(i)}
-OVPaC <- do.call(randomForest::combine, ovpac_list)
-save(OVPaC, file ='/Volumes/data/projects/nei/mcgaughey/eye_var_Pathogenicity/clean_data/OVPaC_RF.Rdata')
 
