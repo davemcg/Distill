e9c5e75fab83e55a79f496e0a66641a68f100248
David McGaughey <mcgaughey@gmail.com>
2018-06-20 07:07:19 -0400

    update paths and output names

--- scripts/merge_models.R
+++ scripts/merge_models.R
@@ -5,7 +5,9 @@ library(randomForest)
 library(tidyverse)
 
 model_merger <- function(pattern='VPaC__6mtry_v2*', mtry=6, pac = 'VPaC_pieces', type='VPaC'){
-  files <- list.files(paste0('/Volumes/data/projects/nei/mcgaughey/eye_var_Pathogenicity/clean_data/', pac), pattern = pattern, full.names = TRUE)
+  files <- list.files(paste0('/data/mcgaugheyd/projects/nei/mcgaughey/eye_var_Pathogenicity/clean_data/', pac), 
+                      pattern = pattern, 
+                      full.names = TRUE)
   for (i in files){load(i)}
   ls()[grepl('VPaC', ls())]
   
@@ -19,17 +21,16 @@ model_merger <- function(pattern='VPaC__6mtry_v2*', mtry=6, pac = 'VPaC_pieces',
   ##############################
   # SAVE MODEL
   ###############################
-  save(list=name, file=paste0('/Volumes/data/projects/nei/mcgaughey/eye_var_Pathogenicity/clean_data/', name, '.Rdata'))
+  save(list=name, file=paste0('/data/mcgaugheyd/projects/nei/mcgaughey/eye_var_Pathogenicity/clean_data/', name, '.Rdata'))
 }
+# 
+# #VPaC
+# model_merger(pattern='VPaC__9mtry_v3*', mtry=9, pac='VPaC_pieces', type='VPaC')
+# v
 
 #VPaC
-model_merger(pattern='VPaC__3mtry_v2*', mtry=3, pac='VPaC_pieces', type='VPaC')
-model_merger(pattern='VPaC__6mtry_v2*', mtry=6, pac='VPaC_pieces', type='VPaC')
-model_merger(pattern='VPaC__10mtry_v2*', mtry=10, pac='VPaC_pieces', type='VPaC')
-
-#OVPaC
-model_merger(pattern='OVPaC__3mtry_v2*', mtry=3, pac='OVPaC_pieces', type='OVPaC')
-model_merger(pattern='OVPaC__6mtry_v2*', mtry=6, pac='OVPaC_pieces', type='OVPaC')
-model_merger(pattern='OVPaC__10mtry_v2*', mtry=10, pac='OVPaC_pieces', type='OVPaC')
+model_merger(pattern='VPaC__9mtry_v4_*', mtry=9, pac='VPaC_pieces', type='VPaC')
+model_merger(pattern='VPaC__12mtry_v4_*', mtry=12, pac='VPaC_pieces', type='VPaC')
+model_merger(pattern='VPaC__15mtry_v4_*', mtry=15, pac='VPaC_pieces', type='VPaC')
 
 
