8daaa8dc67e032643315e155b1b059ec4972951d
David McGaughey <mcgaughey@gmail.com>
2018-06-20 17:11:28 -0400

    swap around paths for biowulf2

--- scripts/build_rawR.R
+++ scripts/build_rawR.R
@@ -1,7 +1,7 @@
 # load raw
 library(tidyverse)
 #gemini_tsvs <- list.files('/Volumes/data/projects/nei/mcgaughey/eye_var_Pathogenicity/data/', recursive = T, pattern='*tsv.gz', full.names = T)
-base_path = '/Volumes/data/projects/nei/mcgaughey/eye_var_Pathogenicity/data/'
+base_path = '/data/mcgaugheyd/projects/nei/mcgaughey/eye_var_Pathogenicity/data/'
 files = c('ddl_nisc_100_panel/ddl_nisc_100_panel.gemini.tsv.gz',
           'gnomad_rare_benign_ish/gnomad_rare_benign_ish.gemini.tsv.gz',
           'grimm/grimm.gemini.tsv.gz',
@@ -18,8 +18,8 @@ for (i in files){
   name <- (i %>% str_split(., '/'))[[1]] %>% tail(1) %>% str_split(.,'\\.') %>% unlist() %>% head(1)
   data_files[[name]] <- read_tsv(paste0(base_path, i), col_types = cols(.default = "c"))
 }
-het <- read_tsv('/Volumes/data/projects/nei/mcgaughey/eye_var_Pathogenicity/data/UK10K/UK10K_EGAD.hets.gz', col_types = cols(.default = "c"))
-hom <- read_tsv('/Volumes/data/projects/nei/mcgaughey/eye_var_Pathogenicity/data/UK10K/UK10K_EGAD.homs.gz', col_types = cols(.default = "c"))
+het <- read_tsv('/data/mcgaugheyd/projects/nei/mcgaughey/eye_var_Pathogenicity/data/UK10K/UK10K_EGAD.hets.gz', col_types = cols(.default = "c"))
+hom <- read_tsv('/data/mcgaugheyd/projects/nei/mcgaughey/eye_var_Pathogenicity/data/UK10K/UK10K_EGAD.homs.gz', col_types = cols(.default = "c"))
 
 raw_data <- bind_rows(data_files, .id = 'DataSet')
 raw_data <- bind_rows(raw_data, 
@@ -28,4 +28,4 @@ raw_data <- bind_rows(raw_data,
 #rm(data_files)
 raw_data <- raw_data %>% mutate(pos_id=paste0(chrom, ':', end, '_', ref, '_', alt))
 
-save(raw_data, file='/Volumes/data/projects/nei/mcgaughey/eye_var_Pathogenicity/data/master/raw_data.Rdata')
+save(raw_data, file='/data/mcgaugheyd/projects/nei/mcgaughey/eye_var_Pathogenicity/data/master/raw_data.Rdata')
