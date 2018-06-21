412354bb98701395393de27bb1bd65ad928aec71
David McGaughey <mcgaughey@gmail.com>
2018-06-20 22:15:14 -0400

    remove unclassified variants

--- scripts/model_assess_builder.R
+++ scripts/model_assess_builder.R
@@ -59,13 +59,14 @@ allX <- raw_data %>%
                              pos_id %in% (raw_data %>% filter(grepl('homsy', DataSet)) %>% pull(pos_id)) ~ 'Homsy',
                              pos_id %in% (raw_data %>% filter(grepl('unifun', DataSet)) %>% pull(pos_id)) ~ 'UniFun',
                              pos_id %in% (raw_data %>% filter(grepl('samocha', DataSet)) %>% pull(pos_id)) ~ 'Samocha',
-                             TRUE ~ DataSet),
+                             TRUE ~ 'Other'),
          Status = case_when(grepl('pathogenic', DataSet_o, ignore.case = T) | grepl('pathogenic', status, ignore.case = T) ~ 'Pathogenic',
                             (DataSet == 'DDL NISC RD Cohort' & pos_id %in% ddl_path_cdot$pos_id) | 
                               (DataSet == 'DDL NISC RD Cohort' & end %in% ddl_path_cdot$End)  ~ 'Pathogenic',
                             TRUE ~ 'NotPathogenic')) %>% 
   mutate_at(vars(one_of(numeric_predictors)), funs(as.numeric(.))) %>% 
-  filter(!grepl('gnomad', DataSet)) 
+  filter(!grepl('gnomad', DataSet)) %>% 
+  filter(DataSet != 'Other')
 
 #rm(raw_data)
 allX$fitcons_float <- allX$fitcons
