41ec14d47dbd649bea047b47fcddb4418d093b92
David McGaughey <mcgaughey@gmail.com>
2018-06-22 14:34:36 -0400

    added nn predictors vector

--- scripts/model_assess_builder.R
+++ scripts/model_assess_builder.R
@@ -13,6 +13,8 @@ library(tidyverse)
 library(caret)
 library(ModelMetrics)
 library(randomForest)
+library(keras)
+library(tensorflow)
 
 # Load processed data and models
 
@@ -45,6 +47,9 @@ load('/data/mcgaugheyd/projects/nei/mcgaughey/eye_var_Pathogenicity/data/master/
 
 
 numeric_predictors<-c('is_exonic','is_coding','is_lof','is_splicing','exon','aa_length','impact_severity','polyphen_score','sift_score','dann','eigen_pc_raw_rankscore','eigen_phred','eigen_raw','eigen_coding_or_noncoding','fathmm_converted_rankscore','fathmm_pred','fathmm_score','gerp','genocanyon_score','genocanyon_score_rankscore','hgmd_overlap','linsight','lrt_omega','lrt_converted_rankscore','lrt_score','m_cap_rankscore','m_cap_score','mpc','metalr_rankscore','metalr_score','metasvm_rankscore','metasvm_score','mutationassessor_score','mutationassessor_score_rankscore','mutationtaster_converted_rankscore','mutationtaster_score','provean_converted_rankscore','provean_score','revel','vest3_rankscore','vest3_score','aaf_1kg_afr','aaf_1kg_all','aaf_1kg_amr','aaf_1kg_eas','aaf_1kg_eur','aaf_1kg_sas','aaf_esp_aa','aaf_esp_all','aaf_esp_ea','ac_exac_afr','ac_exac_all','ac_exac_amr','ac_exac_eas','ac_exac_fin','ac_exac_nfe','ac_exac_oth','ac_exac_sas','adj_exp_lof','adj_exp_mis','adj_exp_syn','af_exac_afr','af_exac_all','af_exac_amr','af_exac_eas','af_exac_nfe','af_exac_oth','af_exac_sas','an_exac_afr','an_exac_all','an_exac_amr','an_exac_eas','an_exac_fin','an_exac_nfe','an_exac_oth','an_exac_sas','ccr_pct_v1','cpg_island','epilogos_bivflnk','epilogos_enh','epilogos_enhbiv','epilogos_enhg','epilogos_het','epilogos_quies','epilogos_reprpc','epilogos_reprpcwk','epilogos_tss','epilogos_tssaflnk','epilogos_tssbiv','epilogos_tx','epilogos_txflnk','epilogos_txwk','epilogos_znf','exac_num_het','exac_num_hom_alt','fathmm_mkl_coding_group','fathmm_mkl_coding_pred','fathmm_mkl_coding_rankscore','fathmm_mkl_coding_score','fitcons','geno2mp','gerp_elements','gno_ac_afr','gno_ac_all','gno_ac_amr','gno_ac_asj','gno_ac_eas','gno_ac_fin','gno_ac_nfe','gno_ac_oth','gno_ac_popmax','gno_af_afr','gno_af_all','gno_af_amr','gno_af_asj','gno_af_eas','gno_af_fin','gno_af_nfe','gno_af_oth','gno_af_popmax','gno_an_afr','gno_an_all','gno_an_amr','gno_an_asj','gno_an_eas','gno_an_fin','gno_an_nfe','gno_an_oth','gno_an_popmax','gno_id','gno_popmax','hapmap1','hapmap2','in_1kg','in_esp','in_exac','lof_z','max_aaf_all','mis_z','n_lof','n_mis','n_syn','pli','pnull','precessive','phylop_100way','segway_mean_score','segway_sum_score','stam_mean','syn_z','grantham','maxentscan','cadd_raw','cadd_phred')
+
+nn_predictors <-c('ccr_pct_v1','cadd_raw','vest3_rankscore','cadd_phred','mis_z','pli','lof_z','phylop_100way','revel','hapmap2','hapmap1','n_mis','epilogos_quies','n_lof','precessive','pnull','adj_exp_lof','adj_exp_syn','dann','adj_exp_mis','syn_z','n_syn','epilogos_txwk','fitcons','m_cap_score','m_cap_rankscore','eigen_phred','eigen_raw','epilogos_tx','is_lof','eigen_pc_raw_rankscore','epilogos_reprpcwk','fathmm_mkl_coding_rankscore','metalr_score','fathmm_mkl_coding_score','metalr_rankscore','impact_severity','metasvm_rankscore','metasvm_score','epilogos_enh','genocanyon_score','fathmm_converted_rankscore','mpc','epilogos_enhg','af_exac_all','epilogos_reprpc','max_aaf_all','mutationassessor_score','gerp','polyphen_score','gerp_elements','mutationassessor_score_rankscore','stam_mean','an_exac_all','af_exac_nfe','provean_converted_rankscore','an_exac_nfe','lrt_score','lrt_omega','grantham','lrt_converted_rankscore','genocanyon_score_rankscore','an_exac_afr','an_exac_amr','an_exac_sas','epilogos_het','ac_exac_all','linsight','gno_an_popmax','exac_num_het','an_exac_eas','gno_an_all','ac_exac_nfe','mutationtaster_converted_rankscore','an_exac_oth','an_exac_fin','gno_an_nfe','gno_af_all','gno_an_afr','epilogos_tssaflnk','gno_af_popmax','epilogos_znf','segway_sum_score','aaf_esp_ea','epilogos_txflnk','provean_score','segway_mean_score','epilogos_tss','aaf_esp_all','af_exac_amr','gno_af_nfe','epilogos_enhbiv','af_exac_sas','sift_score','fathmm_score','ac_exac_amr','aaf_esp_aa','gno_ac_all','gno_af_afr','ac_exac_sas','af_exac_eas','gno_an_fin','af_exac_afr','gno_an_eas','gno_an_oth','gno_ac_nfe','gno_ac_popmax','ac_exac_eas','ac_exac_afr','epilogos_tssbiv','gno_ac_afr','vest3_score')
+
 ############ 
 ###  DDL ###
 ############
@@ -115,8 +120,8 @@ allX$VPaC_m09 <- sqrt(predict(VPaC_9mtry, allX, type='prob')[,1])
 #############################
 library(DMwR)
 set.seed(89345)
-train_sub <- model_data$ML_set__general_TT$train_set %>% dplyr::select(one_of(most_imp_predictors_no_disease_class), 'Status')
-test_sub <- model_data$ML_set__general_TT$test_set %>% dplyr::select(one_of(most_imp_predictors_no_disease_class),'Status')
+train_sub <- model_data$ML_set__general_TT$train_set %>% dplyr::select(one_of(numeric_predictors), 'Status')
+test_sub <- model_data$ML_set__general_TT$test_set %>% dplyr::select(one_of(numeric_predictors),'Status')
 
 train_sub <- SMOTE(Status ~ ., as.data.frame(train_sub))
 status_train <- train_sub$Status
@@ -127,31 +132,31 @@ status_test01 <- case_when(status_test == 'Pathogenic' ~ 1,
                            TRUE ~ 0)
 train_sub <- train_sub %>% dplyr::select(-Status)
 test_sub <- test_sub %>% dplyr::select(-Status)
-mean <- apply(train_sub %>% dplyr::select_(.dots = most_imp_predictors_no_disease_class), 2, mean)
-std <- apply(train_sub %>% dplyr::select_(.dots = most_imp_predictors_no_disease_class), 2, sd)
+mean <- apply(train_sub %>% dplyr::select_(.dots = numeric_predictors), 2, mean)
+std <- apply(train_sub %>% dplyr::select_(.dots = numeric_predictors), 2, sd)
 train_data <- scale(train_sub, center=mean, scale=std)
 test_data <- scale(test_sub, center=mean,scale=std)
 
 # reshape
-dim(train_data) <- c(nrow(train_data),1,length(most_imp_predictors_no_disease_class))
-dim(test_data) <- c(nrow(test_data),1,length(most_imp_predictors_no_disease_class))
+dim(train_data) <- c(nrow(train_data),1,length(numeric_predictors))
+dim(test_data) <- c(nrow(test_data),1,length(numeric_predictors))
 
 model <- keras_model_sequential() %>% 
-  layer_lstm(1.5*length(most_imp_predictors_no_disease_class), recurrent_dropout=0.2, input_shape=c(1, length(most_imp_predictors_no_disease_class)), return_sequences = T) %>%
+  layer_lstm(1.5*length(numeric_predictors), recurrent_dropout=0.2, input_shape=c(1, length(numeric_predictors)), return_sequences = T) %>%
   layer_dropout(0.2) %>%
-  layer_lstm(1.5*length(most_imp_predictors_no_disease_class), recurrent_dropout=0.2, input_shape=c(1, length(most_imp_predictors_no_disease_class)), return_sequences = T) %>%
+  layer_lstm(1.5*length(numeric_predictors), recurrent_dropout=0.2, input_shape=c(1, length(numeric_predictors)), return_sequences = T) %>%
   layer_dropout(0.2) %>%
-  layer_lstm(1.5*length(most_imp_predictors_no_disease_class), recurrent_dropout=0.2, input_shape=c(1, length(most_imp_predictors_no_disease_class)), return_sequences = T) %>%
+  layer_lstm(1.5*length(numeric_predictors), recurrent_dropout=0.2, input_shape=c(1, length(numeric_predictors)), return_sequences = T) %>%
   layer_dropout(0.2) %>%
-  layer_lstm(1.5*length(most_imp_predictors_no_disease_class), recurrent_dropout=0.2, input_shape=c(1, length(most_imp_predictors_no_disease_class)), return_sequences = T) %>%
+  layer_lstm(1.5*length(numeric_predictors), recurrent_dropout=0.2, input_shape=c(1, length(numeric_predictors)), return_sequences = T) %>%
   layer_dropout(0.2) %>%
-  layer_lstm(1.5*length(most_imp_predictors_no_disease_class), recurrent_dropout=0.2, input_shape=c(1, length(most_imp_predictors_no_disease_class)), return_sequences = T) %>%
+  layer_lstm(1.5*length(numeric_predictors), recurrent_dropout=0.2, input_shape=c(1, length(numeric_predictors)), return_sequences = T) %>%
   layer_dropout(0.2) %>%
-  layer_lstm(1.5*length(most_imp_predictors_no_disease_class), recurrent_dropout=0.2, input_shape=c(1, length(most_imp_predictors_no_disease_class)), return_sequences = T) %>%
+  layer_lstm(1.5*length(numeric_predictors), recurrent_dropout=0.2, input_shape=c(1, length(numeric_predictors)), return_sequences = T) %>%
   layer_dropout(0.2) %>%
-  layer_lstm(1.5*length(most_imp_predictors_no_disease_class), recurrent_dropout=0.2, input_shape=c(1, length(most_imp_predictors_no_disease_class)), return_sequences = T) %>%
+  layer_lstm(1.5*length(numeric_predictors), recurrent_dropout=0.2, input_shape=c(1, length(numeric_predictors)), return_sequences = T) %>%
   layer_dropout(0.2) %>%
-  layer_lstm(1.5*length(most_imp_predictors_no_disease_class), recurrent_dropout=0.2, input_shape=c(1, length(most_imp_predictors_no_disease_class))) %>%
+  layer_lstm(1.5*length(numeric_predictors), recurrent_dropout=0.2, input_shape=c(1, length(numeric_predictors))) %>%
   layer_dropout(0.1) %>%
   layer_dense(units=1, activation='sigmoid')
 
@@ -167,7 +172,7 @@ DeepRNN <- list()
 DeepRNN$model <- model
 DeepRNN$mean <- mean
 DeepRNN$std <- std
-DeepRNN$predictors <- most_imp_predictors_no_disease_class
+DeepRNN$predictors <- numeric_predictors
 # save_model_hdf5(model, "/Volumes/data/projects/nei/mcgaughey/eye_var_Pathogenicity/clean_data/DeepRNN_2018_06_22.h5")
 # save(DeepRNN, file='/Volumes/data/projects/nei/mcgaughey/eye_var_Pathogenicity/clean_data/DeepRNN_2018_06_22.Rdata')
 
@@ -232,7 +237,7 @@ train_set$DeepVPaC <- predict(DeepVPaC, train_set, type='prob')[,1]
 
 # predict DeepVPaC on allX
 # but first, scale data for DeepRNN model to work on
-all_sub <- allX %>% select_(.dots=most_imp_predictors_no_disease_class)
+all_sub <- allX %>% select_(.dots=numeric_predictors)
 all_sub$DeepRNN <- scale_predict(all_sub, model, DeepRNN$predictors, DeepRNN$mean, DeepRNN$std)
 all_sub$VPaC_m15 <- allX$VPaC_m15
 all_sub$DeepVPaC <- predict(DeepVPaC, all_sub, type='prob')[,1]
