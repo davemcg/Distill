b2b93450ccbf1741395979673ed439d670095119
David McGaughey <mcgaughey@gmail.com>
2018-07-13 12:28:17 -0400

    uses gemini dbs to build now

--- scripts/build_rawR.R
+++ scripts/build_rawR.R
@@ -1,20 +1,29 @@
-# load raw
+# create gemini output, then merge together
 library(tidyverse)
 #gemini_tsvs <- list.files('/Volumes/data/projects/nei/mcgaughey/eye_var_Pathogenicity/data/', recursive = T, pattern='*tsv.gz', full.names = T)
 base_path = '/data/mcgaugheyd/projects/nei/mcgaughey/eye_var_Pathogenicity/data/'
-files = c('ddl_nisc_100_panel/ddl_nisc_100_panel.gemini.tsv.gz',
-          'gnomad_rare_benign_ish/gnomad_rare_benign_ish.gemini.tsv.gz',
-          'grimm/grimm.gemini.tsv.gz',
-          'homsy_benign/homsy_benign.gemini.tsv.gz',
-          'homsy_pathogenic/homsy_pathogenic.gemini.tsv.gz',
-          'samocha_benign/samocha_benign.gemini.tsv.gz',
-          'samocha_pathogenic/samocha_pathogenic.gemini.tsv.gz',
-          'unifun_benign/unifun_benign.gemini.tsv.gz',
-          'unifun_pathogenic/unifun_pathogenic.gemini.tsv.gz',
-          'clinvar/clinvar.gemini.tsv.gz')
+
+
+
+gemini_dbs = c('ddl_nisc_100_panel/DDL_NISC_targeted_panel.PED_ddl_nisc.gemini.db',
+          # 'gnomad_rare_benign_ish/gnomad.exomes.r2.0.2.sites.maxAF_0.01_20percent.PED_faux.gemini.db',
+          'grimm/grimm.PED_faux.gemini.db',
+          'homsy_benign/homsy.benign.PED_faux.gemini.db',
+          'homsy_pathogenic/homsy.pathogenic.PED_faux.gemini.db',
+          'ogvfb/VCFs.GATK.PED_master.gemini.db',
+          'samocha_benign/samocha.benign.PED_faux.gemini.db',
+          'samocha_pathogenic/samocha.pathogenic.PED_faux.gemini.db',
+          'unifun_benign/UniFun_benign.fakeS.PED_faux.gemini.db',
+          'unifun_pathogenic/UniFun_deleterious.fakeS.PED_faux.gemini.db',
+          'clinvar/clinvar_RD.PED_faux.gemini.db')
+
+for (gemini_db in gemini_dbs){
+  system(paste0("time gemini query --header -q \"SELECT * from variants\" ", base_path, gemini_db, "| bgzip > ", base_path, gsub('.db','.tsv.gz', gemini_db)))
+  print(paste(gemini_db, 'done'))
+}
 
 data_files = list()
-for (i in files){
+for (i in gsub('.db','.tsv.gz', gemini_dbs)){
   name <- (i %>% str_split(., '/'))[[1]] %>% tail(1) %>% str_split(.,'\\.') %>% unlist() %>% head(1)
   data_files[[name]] <- read_tsv(paste0(base_path, i), col_types = cols(.default = "c"))
 }
@@ -28,4 +37,4 @@ raw_data <- bind_rows(raw_data,
 #rm(data_files)
 raw_data <- raw_data %>% mutate(pos_id=paste0(chrom, ':', end, '_', ref, '_', alt))
 
-save(raw_data, file='/data/mcgaugheyd/projects/nei/mcgaughey/eye_var_Pathogenicity/data/master/raw_data.Rdata')
+save(raw_data, file='/data/mcgaugheyd/projects/nei/mcgaughey/eye_var_Pathogenicity/data/master/raw_data_2018_07_13.Rdata')
