e6fa9194e14847b7a9bb3a58fa0072271e8ebb84
David McGaughey <mcgaughey@gmail.com>
2018-07-17 15:04:46 -0400

    update for new

--- scripts/merge_models.R
+++ scripts/merge_models.R
@@ -4,7 +4,7 @@
 library(randomForest)
 library(tidyverse)
 
-model_merger <- function(pattern='VPaC__6mtry_v2*', mtry=6, pac = 'VPaC_pieces', type='VPaC'){
+model_merger <- function(pattern='VPaC__6mtry_v2*', mtry=6, pac = 'VPaC_pieces', type='VPaC', version='v1'){
   files <- list.files(paste0('/data/mcgaugheyd/projects/nei/mcgaughey/eye_var_Pathogenicity/clean_data/', pac), 
                       pattern = pattern, 
                       full.names = TRUE)
@@ -16,7 +16,7 @@ model_merger <- function(pattern='VPaC__6mtry_v2*', mtry=6, pac = 'VPaC_pieces',
   PaC <- do.call(randomForest::combine, pac_list)
   
   
-  name = paste0(type, '_', mtry, 'mtry')
+  name = paste0(type, '_', mtry, 'mtry', '_', version)
   assign(name, PaC)
   ##############################
   # SAVE MODEL
@@ -29,8 +29,8 @@ model_merger <- function(pattern='VPaC__6mtry_v2*', mtry=6, pac = 'VPaC_pieces',
 # v
 
 #VPaC
-model_merger(pattern='VPaC__9mtry_v6_*', mtry=9, pac='VPaC_pieces', type='VPaC')
-model_merger(pattern='VPaC__12mtry_v6_*', mtry=12, pac='VPaC_pieces', type='VPaC')
-model_merger(pattern='VPaC__15mtry_v6_*', mtry=15, pac='VPaC_pieces', type='VPaC')
+model_merger(pattern='VPaC__06mtry_v7_*', mtry=6, pac='VPaC_pieces', type='VPaC', version = 'v8')
+model_merger(pattern='VPaC__09mtry_v7_*', mtry=9, pac='VPaC_pieces', type='VPaC', version = 'v8')
+model_merger(pattern='VPaC__12mtry_v7_*', mtry=12, pac='VPaC_pieces', type='VPaC', version = 'v8')
 
 
