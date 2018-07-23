f398349e440e86803abb24265c418924762b2106
David McGaughey <mcgaughey@gmail.com>
2018-07-23 16:34:23 -0400

    update for UK10K split of train and test

--- scripts/build_data.R
+++ scripts/build_data.R
@@ -30,7 +30,7 @@ info <- c('pos_id',
 cat_predictors <- c('genesplicer',
                     'spliceregion')
 
-numeric_predictors<-c('is_exonic','is_coding','is_lof','is_splicing','exon','aa_length','impact_severity','polyphen_score','sift_score','dann','eigen_pc_raw_rankscore','eigen_phred','eigen_raw','eigen_coding_or_noncoding','fathmm_converted_rankscore','fathmm_pred','fathmm_score','gerp','genocanyon_score','genocanyon_score_rankscore','hgmd_overlap','linsight','lrt_omega','lrt_converted_rankscore','lrt_score','m_cap_rankscore','m_cap_score','mpc','metalr_rankscore','metalr_score','metasvm_rankscore','metasvm_score','mutationassessor_score','mutationassessor_score_rankscore','mutationtaster_converted_rankscore','mutationtaster_score','provean_converted_rankscore','provean_score','revel','vest3_rankscore','vest3_score','aaf_1kg_afr','aaf_1kg_all','aaf_1kg_amr','aaf_1kg_eas','aaf_1kg_eur','aaf_1kg_sas','aaf_esp_aa','aaf_esp_all','aaf_esp_ea','ac_exac_afr','ac_exac_all','ac_exac_amr','ac_exac_eas','ac_exac_fin','ac_exac_nfe','ac_exac_oth','ac_exac_sas','adj_exp_lof','adj_exp_mis','adj_exp_syn','af_exac_afr','af_exac_all','af_exac_amr','af_exac_eas','af_exac_nfe','af_exac_oth','af_exac_sas','an_exac_afr','an_exac_all','an_exac_amr','an_exac_eas','an_exac_fin','an_exac_nfe','an_exac_oth','an_exac_sas','ccr_pct_v1','cpg_island','epilogos_bivflnk','epilogos_enh','epilogos_enhbiv','epilogos_enhg','epilogos_het','epilogos_quies','epilogos_reprpc','epilogos_reprpcwk','epilogos_tss','epilogos_tssaflnk','epilogos_tssbiv','epilogos_tx','epilogos_txflnk','epilogos_txwk','epilogos_znf','exac_num_het','exac_num_hom_alt','fathmm_mkl_coding_group','fathmm_mkl_coding_pred','fathmm_mkl_coding_rankscore','fathmm_mkl_coding_score','fitcons','geno2mp','gerp_elements','gno_ac_afr','gno_ac_all','gno_ac_amr','gno_ac_asj','gno_ac_eas','gno_ac_fin','gno_ac_nfe','gno_ac_oth','gno_ac_popmax','gno_af_afr','gno_af_all','gno_af_amr','gno_af_asj','gno_af_eas','gno_af_fin','gno_af_nfe','gno_af_oth','gno_af_popmax','gno_an_afr','gno_an_all','gno_an_amr','gno_an_asj','gno_an_eas','gno_an_fin','gno_an_nfe','gno_an_oth','gno_an_popmax','gno_id','gno_popmax','hapmap1','hapmap2','in_1kg','in_esp','in_exac','lof_z','max_aaf_all','mis_z','n_lof','n_mis','n_syn','pli','pnull','precessive','phylop_100way','segway_mean_score','segway_sum_score','stam_mean','syn_z','grantham','maxentscan','cadd_raw','cadd_phred', 'sigmaaf_lof_0001', 'sigmaaf_lof_01', 'sigmaaf_missense_0001', 'sigmaaf_missense_01')
+numeric_predictors<-c('is_exonic','is_coding','is_lof','is_splicing','exon','aa_length','impact_severity','polyphen_score','sift_score','dann','eigen_phred','eigen_coding_or_noncoding','fathmm_converted_rankscore','fathmm_pred','gerp','genocanyon_score_rankscore','hgmd_overlap','linsight','lrt_omega','lrt_converted_rankscore','m_cap_rankscore','mpc','metalr_rankscore','metasvm_rankscore','mutationassessor_score_rankscore','mutationtaster_converted_rankscore','provean_converted_rankscore','provean_score','revel','vest3_rankscore','aaf_1kg_afr','aaf_1kg_all','aaf_1kg_amr','aaf_1kg_eas','aaf_1kg_eur','aaf_1kg_sas','aaf_esp_aa','aaf_esp_all','aaf_esp_ea','ac_exac_afr','ac_exac_all','ac_exac_amr','ac_exac_eas','ac_exac_fin','ac_exac_nfe','ac_exac_oth','ac_exac_sas','adj_exp_lof','adj_exp_mis','adj_exp_syn','af_exac_afr','af_exac_all','af_exac_amr','af_exac_eas','af_exac_nfe','af_exac_oth','af_exac_sas','an_exac_afr','an_exac_all','an_exac_amr','an_exac_eas','an_exac_fin','an_exac_nfe','an_exac_oth','an_exac_sas','ccr_pct_v1','cpg_island','epilogos_bivflnk','epilogos_enh','epilogos_enhbiv','epilogos_enhg','epilogos_het','epilogos_quies','epilogos_reprpc','epilogos_reprpcwk','epilogos_tss','epilogos_tssaflnk','epilogos_tssbiv','epilogos_tx','epilogos_txflnk','epilogos_txwk','epilogos_znf','exac_num_het','exac_num_hom_alt','fathmm_mkl_coding_group','fathmm_mkl_coding_pred','fathmm_mkl_coding_rankscore','fitcons','geno2mp','gerp_elements','gno_ac_afr','gno_ac_all','gno_ac_amr','gno_ac_asj','gno_ac_eas','gno_ac_fin','gno_ac_nfe','gno_ac_oth','gno_ac_popmax','gno_af_afr','gno_af_all','gno_af_amr','gno_af_asj','gno_af_eas','gno_af_fin','gno_af_nfe','gno_af_oth','gno_af_popmax','gno_an_afr','gno_an_all','gno_an_amr','gno_an_asj','gno_an_eas','gno_an_fin','gno_an_nfe','gno_an_oth','gno_an_popmax','gno_id','gno_popmax','hapmap1','hapmap2','in_1kg','in_esp','in_exac','lof_z','max_aaf_all','mis_z','n_lof','n_mis','n_syn','pli','pnull','precessive','phylop_100way','segway_mean_score','segway_sum_score','stam_mean','syn_z','grantham','maxentscan','cadd_phred', 'sigmaaf_lof_0001', 'sigmaaf_lof_01', 'sigmaaf_missense_0001', 'sigmaaf_missense_01')
 
 ###############################
 # UK10K processing
@@ -77,7 +77,18 @@ all_PATH <- all_set__uk10k %>% filter(Status=='Pathogenic')
 set.seed(115470)
 all_NOT_PATH__CUT <- all_set__uk10k %>% filter(Status=='NotPathogenic') #%>% sample_n(20000)
 
-ML_set__UK10K <- rbind(all_PATH, all_NOT_PATH__CUT)
+
+#####
+# split train and test
+#####
+
+ML_set__UK10K <- rbind(all_PATH %>% filter(DataSet == 'UK10 Train'), all_NOT_PATH__CUT %>% filter(DataSet == 'UK10 Train'))
+Test_set__UK10K <- rbind(all_PATH %>% filter(DataSet == 'UK10 Test'), all_NOT_PATH__CUT %>% filter(DataSet == 'UK10 Test'))
+
+
+
+
+
 print('UK10K Loaded')
 ###########################################
 # ClinVar Processing
@@ -181,77 +192,6 @@ gnomad_processed_other <- gnomad_processed %>% filter(!pos_id %in% c(gnomad_proc
 #gnomad_processed_sub$Source <- 'gnomAD'
 #gnomad_processed_other$Source <- 'gnomAD'
 print('gnomAD Loaded')
-
-# ################################################
-# # Build ClinVar spread data
-# ################################################
-# spread <- fread(paste0('gzcat ', spread_file))
-# 
-# ## Prep data for modeling
-# spread_processed <- spread %>% 
-#   #filter(status!='PATHOGENIC_OTHER') %>% # drop non eye pathogenic variants for the model learning 
-#   #separate(gene_eyediseaseclass, c('RDGene','DiseaseClass'), sep='_') %>%  #split off RD disease type
-#   #select(-RDGene) %>% 
-#   mutate(pos_id=paste0(chrom, ':', end, '_', ref, '_', alt),
-#          impact_severity = case_when(impact_severity == 'HIGH' ~ 3, # convert to integer 
-#                                      impact_severity == 'MED' ~ 2, 
-#                                      TRUE ~ 1),
-#          Status = factor('SPREAD'),
-#          genesplicer = case_when(genesplicer == "" ~ 'No',
-#                                  grepl('^gain', genesplicer) ~ 'Gain',
-#                                  grepl('^loss', genesplicer) ~ 'Loss',
-#                                  grepl('^diff', genesplicer) ~ 'Diff',
-#                                  TRUE ~ 'Else')) %>% 
-#   mutate_at(vars(matches('ac_|an_|^n_')), funs(as.integer(.))) %>% # convert columns with ac_|whatever to integer (ac is allele count)
-#   mutate_at(vars(matches('af_|dann|revel|mpc|gerp|polyphen_score|sift_score|fitcons|gerp_elements|^adj|_z$|^pli$|^pnull$|precessive|^phylop_100|linsight|_rankscore$|ccr_pct_v1|epilogos|segway')), funs(as.numeric(.))) %>%  # af is allele frequency
-#   select(pos_id,
-#          Status,
-#          is_exonic,
-#          is_coding,
-#          is_lof,
-#          is_splicing,
-#          impact_severity,
-#          polyphen_score,
-#          sift_score,
-#          dann,
-#          gerp_elements,
-#          mpc,
-#          revel,
-#          max_aaf_all,
-#          gno_ac_afr,
-#          gno_ac_eas,
-#          gno_ac_all,
-#          gno_ac_popmax,
-#          ac_exac_sas,
-#          ac_exac_fin,
-#          aaf_1kg_all,
-#          aaf_esp_all,
-#          ac_exac_all,
-#          ac_exac_amr,
-#          ac_exac_oth,
-#          gno_af_all,
-#          gno_an_popmax,
-#          an_exac_all,
-#          af_exac_all,
-#          fitcons,
-#          linsight,
-#          lof_z:precessive,
-#          phylop_100way,
-#          grantham,
-#          cadd_phred,
-#          contains("_rankscore"),
-#          contains("segway"),
-#          contains("epilogos"),
-#          ccr_pct_v1,
-#          genesplicer,
-#          spliceregion)
-# 
-# # fill missing with -1
-# spread_processed[is.na(spread_processed)] <- -1
-# 
-# ML_set__spread <- spread_processed 
-# 
-# print('ClinVar Spread Loaded')
 
 ################################################
 # Combine UK10K and ClinVar and gnomAD data
@@ -387,9 +327,10 @@ model_data <- list()
 model_data$ML_set__eye_TT <- ML_set__eye_TT
 model_data$ML_set__general_TT <- ML_set__general_TT
 model_data$ML_set__other_TT <- ML_set__other_TT
+model_data$Test_set__UK10K <- Test_set__UK10K
 model_data$pos_id__source <- pos_id__source
 model_data$predictors <- numeric_predictors
 model_data$sessionInfo <- sessionInfo()
-save(model_data, file='/data/mcgaugheyd/projects/nei/mcgaughey/eye_var_Pathogenicity/clean_data/model_data_2018_07_13.Rdata')
+save(model_data, file='/data/mcgaugheyd/projects/nei/mcgaughey/eye_var_Pathogenicity/clean_data/model_data_2018_07_23.Rdata')
 
 #save(clinvar_spread, file='/data/mcgaugheyd/projects/nei/mcgaughey/eye_var_Pathogenicity/clean_data/model_spread.Rdata')
