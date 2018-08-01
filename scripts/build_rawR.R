e27251a8c279d9daec3f49fbdb6bd4456ccecf85
David McGaughey <mcgaughey@gmail.com>
2018-08-01 12:49:26 -0400

    splits the gemini call between filtering (to remove common var) and not filtering (prebuilt sets like samocha)

--- scripts/build_rawR.R
+++ scripts/build_rawR.R
@@ -5,25 +5,37 @@ base_path = '/data/mcgaugheyd/projects/nei/mcgaughey/eye_var_Pathogenicity/data/
 
 
 
-gemini_dbs = c('ddl_nisc_100_panel/DDL_NISC_targeted_panel.PED_ddl_nisc.gemini.db',
-          # 'gnomad_rare_benign_ish/gnomad.exomes.r2.0.2.sites.maxAF_0.01_20percent.PED_faux.gemini.db',
+gemini_dbs = c(#'ddl_nisc_100_panel/DDL_NISC_targeted_panel.PED_ddl_nisc.gemini.db',
+          #'gnomad_rare_benign_ish/gnomad.exomes.r2.0.2.sites.maxAF_0.01_20percent.PED_faux.gemini.db',
           'grimm/grimm.PED_faux.gemini.db',
           'homsy_benign/homsy.benign.PED_faux.gemini.db',
           'homsy_pathogenic/homsy.pathogenic.PED_faux.gemini.db',
-          'ogvfb/VCFs.GATK.PED_master.gemini.db',
+          #'ogvfb/VCFs.GATK.PED_master.gemini.db',
           'samocha_benign/samocha.benign.PED_faux.gemini.db',
           'samocha_pathogenic/samocha.pathogenic.PED_faux.gemini.db',
           'unifun_benign/UniFun_benign.fakeS.PED_faux.gemini.db',
-          'unifun_pathogenic/UniFun_deleterious.fakeS.PED_faux.gemini.db',
-          'clinvar/clinvar_RD.PED_faux.gemini.db')
+          'unifun_pathogenic/UniFun_deleterious.fakeS.PED_faux.gemini.db')
+          #'clinvar/clinvar_RD.PED_faux.gemini.db')
+
+gemini_dbs_filter <- c('ddl_nisc_100_panel/DDL_NISC_targeted_panel.PED_ddl_nisc.gemini.db',
+                       'gnomad_rare_benign_ish/gnomad.exomes.r2.0.2.sites.maxAF_0.01_20percent.PED_faux.gemini.db',
+                       'ogvfb/VCFs.GATK.PED_master.gemini.db',
+                       'clinvar/clinvar_RD.PED_faux.gemini.db')
+          
 
 for (gemini_db in gemini_dbs){
   system(paste0("time gemini query --header -q \"SELECT * from variants\" ", base_path, gemini_db, "| bgzip > ", base_path, gsub('.db','.tsv.gz', gemini_db)))
   print(paste(gemini_db, 'done'))
 }
 
+
+for (gemini_db in gemini_dbs_filter){
+  system(paste0("time gemini query --header -q \"SELECT * from variants WHERE (aaf_esp_all < 0.01 AND aaf_1kg_all < 0.01 AND af_exac_all < 0.01  AND (is_coding=1 OR is_splicing=1)) OR impact_severity='HIGH' OR clinvar_sig LIKE '%patho%'\" ", base_path, gemini_db, "| bgzip > ", base_path, gsub('.db','.tsv.gz', gemini_db)))
+  print(paste(gemini_db, 'done'))
+}
+
 data_files = list()
-for (i in gsub('.db','.tsv.gz', gemini_dbs)){
+for (i in gsub('.db','.tsv.gz', c(gemini_dbs, gemini_dbs_filter))){
   name <- (i %>% str_split(., '/'))[[1]] %>% tail(2) %>% str_split(.,'\\.') %>% unlist() %>% head(1)
   print(name)
   data_files[[name]] <- read_tsv(paste0(base_path, i), col_types = cols(.default = "c"))
@@ -38,4 +50,4 @@ raw_data <- bind_rows(raw_data,
 #rm(data_files)
 raw_data <- raw_data %>% mutate(pos_id=paste0(chrom, ':', end, '_', ref, '_', alt))
 
-save(raw_data, file='/data/mcgaugheyd/projects/nei/mcgaughey/eye_var_Pathogenicity/data/master/raw_data_2018_07_13.Rdata')
+save(raw_data, file='/data/mcgaugheyd/projects/nei/mcgaughey/eye_var_Pathogenicity/data/master/raw_data_2018_08_01.Rdata')
